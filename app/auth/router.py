baca2907a353f44c5b6763a862691b15cec83a43
iristianyiz <iristianyi720@gmail.com>
2024-12-03 23:31:12 -0800

    Update router.py

--- app/auth/router.py
+++ app/auth/router.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-module-docstring, pointless-string-statement, no-self-argument, too-few-public-methods, missing-final-newline
 from datetime import datetime, timedelta
 from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException, status
